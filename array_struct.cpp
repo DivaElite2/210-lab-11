5ad1e84491f17a35dc67102bc65f653e5d2ea5fb
Lateefah camacho <lateefah.camacho07@gmail.com>
2025-09-20 15:03:39 -0700

    check for errors

--- array_struct.cpp
+++ array_struct.cpp
@@ -3,7 +3,17 @@
 #include <iostream>
 using namespace std;
 
+// Student structure
 struct Student {
     int grades;
-    
+    int id;
+    int name;
+};
+
+int main()
+{
+   //declare an array of structures
+   int size = 3;
+   Student stu[size];
 }
+
